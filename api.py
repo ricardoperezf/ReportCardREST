5fa008505fa3fd91d9ba62328132d0147016e5e5
ricardoperezf <ricardojpf98@gmail.com>
2017-12-20 07:38:42 +0000

    Initialize app

--- api.py
+++ api.py
@@ -1,1 +1,11 @@
-print("Hello world! Im Ricardo and Javier now. Second changes.Thrid change.Four change. Five change")
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return 'Hello from Flask!'
+
+if __name__ == '__main__':
+  app.run(debug = True)
