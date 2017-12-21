db0196e9dfd0036609f83b6d485d48066124f892
ricardoperezf <ricardojpf98@gmail.com>
2017-12-21 13:55:17 -0600

    Deleted test of endpoint

--- test/test_case.py
+++ test/test_case.py
@@ -1,13 +1,17 @@
 import sys
+
 sys.path.append('/home/ricardo/ws/ReportCard/ReportCard')
 import unittest
 import api
 
 
 class TestApi(unittest.TestCase):
+    obtained_result = ""
+    expected_result = ""
+
     def test_get_user_grade(self):
         obtained_result = api.hello_world()
-        expected_result =  'Hello from Flask! Im RicDevv'
+        expected_result = 'Hello from Flask! Im RicDev'
         self.assertEqual(expected_result, obtained_result)
 
 
