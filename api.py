05879ba7966476c2f4f7a288bedef4132cc9cce8
ricardoperezf <ricardojpf98@gmail.com>
2017-12-21 13:54:41 -0600

    Change structure of api.py successfully

--- api.py
+++ api.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'abcdefgh'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 
 vector_grades = {'nombre': 'Ricardo', 'course': 'Web Services', 'grade': 100}
 
-def example():
-    return 10
 
 @app.route('/')
 def hello_world():
@@ -21,22 +24,23 @@ def get_grades():
     global vector_grades
     return "<h1>Nombre: %s</h1>" % vector_grades['nombre']
 
-
-@app.route('/register')
-def register_new_user():
+g
+@app.route('/register', methods=['POST'])
+def new_user():
     username = request.json.get('username')
     password = request.json.get('password')
-    print("\n" + str(username) + " " + str(password) + "\n")
     if username is None or password is None:
-        abort(400)  # MISSING USERNAME OR PASSWORD.
+        abort(400)  # missing arguments
     if user.User.query.filter_by(username=username).first() is not None:
-        abort(400)  # ALREADY EXISTING USER.
-    new_user = user.User(username=username)
-    new_user.hash(password)
-    user.db.session.add(user)
-    user.db.session.commit()
-    return jsonify({'username': user.username}), 201
+        abort(400)  # existing user
+    new_user_db = user.User(username=username)
+    new_user_db.hash(password)
+    db.session.add(new_user_db)
+    db.session.commit()
+    return jsonify({'username': new_user_db.username}), 201
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    if not os.path.exists('db.sqlite'):
+        user.db.create_all()
+    app.run(host="0.0.0.0", port=5000)
