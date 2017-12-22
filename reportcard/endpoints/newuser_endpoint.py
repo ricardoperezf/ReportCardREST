eab3e5b87a2c19da8b2da2774127b5d93e5ffd48
ricardoperezf <ricardojpf98@gmail.com>
2017-12-21 20:03:13 -0600

    Added new user's endpoint complete

--- reportcard/endpoints/newuser_endpoint.py
+++ reportcard/endpoints/newuser_endpoint.py
@@ -1,19 +1,6 @@
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from passlib.apps import custom_app_context as pwd_context
-from reportcard import reportcard_app
-
-db = SQLAlchemy(reportcard_app)
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), index=True)
-    password_hashed = db.Column(db.String(64))
-
-    def hash(self, password):  # ENCRYPTAR LA CONTRASEÑA.
-        self.password_hashed = pwd_context.encrypt(password)
+from reportcard import reportcard_app, db
+from ..models.user import User
 
 
 @reportcard_app.route('/register', methods=['POST'])
@@ -30,7 +17,3 @@ def register_new_user():
     db.session.add(user)
     db.session.commit()
     return jsonify({'username': user.username}), 201
-
-@reportcard_app.route('/si')
-def test():
-    return "This is just a test"
