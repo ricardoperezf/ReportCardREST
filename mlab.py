f00dcdebae464566a9563badb875c9d6a41fe06f
ricardoperezf <ricardojpf98@gmail.com>
2017-12-25 21:15:22 -0600

    Add mongodb's connection test

--- mlab.py
+++ mlab.py
@@ -1,2 +1,19 @@
 from flask import Flask
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config['MONGO_DBNAME'] = "reportcard"
+app.config['MONGO_URI'] = "mongodb://ricardo:ricardomongodb@ds163826.mlab.com:63826/reportcard"
+
+mongo = PyMongo(app)
+
+
+@app.route("/add")
+def example_mongo_db():
+    new_grade = mongo.db.grades
+    new_grade.insert({'course': 'Xamarin', 'grade': 100})
+    return "Added thanks God."
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
